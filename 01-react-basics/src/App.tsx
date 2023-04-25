abc459be6da1f46046991f06930cb6ad0471546d
Johan Nordström <johan@digitalvillage.se>
2023-04-25 14:34:00 +0200

    Render a list 🤘🏻.

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,9 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 
+type Post = {
+	id: number
+	title: string
+	likes: number
+}
+
 const App = () => {
 	const [msg, setMsg] = useState("Hi mom, I'm stateful")
 	const [clicks, setClicks] = useState(0)
+	const [posts, setPosts] = useState<Post[]>([
+		{ id: 1, title: "React Rocks 🤘🏻!", likes: 1337 },
+		{ id: 2, title: "JSX Rocks Even Moar 🤘🏻!", likes: 42 },
+		{ id: 3, title: "Got state?", likes: 3 },
+	])
 
 	const handleButtonClick = () => {
 		setClicks(clicks + 1)
@@ -23,6 +34,20 @@ const App = () => {
 			<button onClick={handleButtonClick} className="btn btn-success btn-lg">👆🏻 me!</button>
 
 			<button onClick={ () => { setMsg('Hi dad!') } } className="btn btn-warning btn-lg">Hi dad!</button>
+
+			<hr />
+
+			<h2>Posts</h2>
+
+			<ul>
+				{
+					posts.map(post => (
+						<li key={post.id}>
+							{post.title} ({post.likes} likes)
+						</li>
+					))
+				}
+			</ul>
 		</div>
 	)
 }
