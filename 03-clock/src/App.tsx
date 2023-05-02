9cf79626a705a9a41fed9af827d9192ac5830de4
Johan Nordström <johan@digitalvillage.se>
2023-05-02 14:48:25 +0200

    Working clock.

--- 03-clock/src/App.tsx
+++ 03-clock/src/App.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react'
 import './assets/scss/App.scss'
 
 function App() {
+	const [time, setTime] = useState(() => {
+		console.log("I'm initing")
+		return new Date().toLocaleTimeString()
+	})
+
+	useEffect(() => {
+		console.log("Starting clock...")
+
+		setInterval(() => {
+			setTime(new Date().toLocaleTimeString())
+			console.log("tick")
+		}, 1000)
+	}, [])
 
 	return (
 		<div className="container">
 			<div className="display-1 text-center">
-				00:13:37
+				{time}
 			</div>
 		</div>
 	)
