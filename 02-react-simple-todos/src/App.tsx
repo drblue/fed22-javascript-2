cbfd97517823976399a4670aff2f10ff3852d436
Johan Nordström <johan@digitalvillage.se>
2023-05-02 09:48:04 +0200

    Empty React skeleton with a few files deleted.

---
+++ 02-react-simple-todos/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './assets/scss/App.scss'
+
+function App() {
+	return (
+		<div className="container">
+			<h1>React Simple Todos</h1>
+		</div>
+	)
+}
+
+export default App
