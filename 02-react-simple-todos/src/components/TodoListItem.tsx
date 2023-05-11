18942346f124db044c66f6f11e0c07e04b8d6274
Johan Nordström <johan@digitalvillage.se>
2023-05-11 12:02:35 +0200

    Pass functions to component as props (will be executed in the scope of the parent 👨‍👩‍👧).

--- 02-react-simple-todos/src/components/TodoListItem.tsx
+++ 02-react-simple-todos/src/components/TodoListItem.tsx
@@ -3,24 +3,25 @@ import { Todo } from '../types'
 
 interface IProps {
 	todo: Todo
+	onDelete: (todoToDelete: Todo) => void
+	onToggle: (todo: Todo) => void
 }
 
-const TodoListItem: React.FC<IProps> = ({ todo }) => {
-
+const TodoListItem: React.FC<IProps> = ({ todo, onDelete, onToggle }) => {
 	return (
 		<li className={todo.completed ? 'done' : ''}>
 			<span className="todo-title">
 				{todo.title}
 			</span>
 
-			{/* <span className="ms-1">
-				<span className="todo-toggle" onClick={() => toggleTodo(todo)} role="button">
+			<span className="ms-1">
+				<span className="todo-toggle" onClick={() => onToggle(todo)} role="button">
 					{todo.completed ? '☑️' : '✅'}
 				</span>
-				<span className="todo-delete" onClick={() => deleteTodo(todo)} role="button">
+				<span className="todo-delete" onClick={() => onDelete(todo)} role="button">
 					🗑️
 				</span>
-			</span> */}
+			</span>
 		</li>
 	)
 }
