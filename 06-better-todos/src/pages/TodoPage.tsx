4557db874b236ad17402b55f8a5d1426af836f8c
Johan Nordström <johan@digitalvillage.se>
2023-05-26 15:54:47 +0200

    Get a single todo from route-parameter.

--- 06-better-todos/src/pages/TodoPage.tsx
+++ 06-better-todos/src/pages/TodoPage.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import { Link, useParams } from 'react-router-dom'
+import { Todo } from '../types'
+import * as TodosAPI from '../services/TodosAPI'
 
 const TodoPage = () => {
+	const [todo, setTodo] = useState<Todo|null>(null)
 	const { id } = useParams()
+	const todoId = Number(id)
+
+	// Get todo from API
+	const getTodo = async (id: number) => {
+		// call TodosAPI
+		const data = await TodosAPI.getTodo(id)
+
+		// update todo state with data
+		setTodo(data)
+	}
+
+	useEffect(() => {
+		if (typeof todoId !== "number") {
+			return
+		}
+
+		getTodo(todoId)
+	}, [todoId])
+
+	if (!todo) {
+		return (<p>Loading...</p>)
+	}
 
 	return (
-		<div>Todo ID {id}</div>
+		<>
+			<h1>{todo.title}</h1>
+
+			<p><strong>Status:</strong> {todo.completed ? 'Completed' : 'Not completed'}</p>
+
+			<Link to="/todos">
+				<Button variant='secondary'>&laquo; All todos</Button>
+			</Link>
+		</>
 	)
 }
 
