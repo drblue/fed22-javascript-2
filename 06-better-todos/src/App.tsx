de718af1e61e8a6e30d6571cca134aa2ff8ac59a
Johan Nordström <johan@digitalvillage.se>
2023-05-26 14:47:17 +0200

    Route for Page not found.

--- 06-better-todos/src/App.tsx
+++ 06-better-todos/src/App.tsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Navigation from './components/Navigation'
+import NotFound from './pages/NotFound'
 import TodosPage from './pages/TodosPage'
 import './assets/scss/App.scss'
 
@@ -14,6 +15,8 @@ const App = () => {
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/todos" element={<TodosPage />} />
+
+					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</Container>
 		</div>
