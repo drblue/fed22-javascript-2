5c3dac0b0680d217c66dc89980c16d049a973ef7
Johan Nordström <johan@digitalvillage.se>
2023-05-26 10:40:56 +0200

    React Weather - Don't allow searching with less than 3 characters.

--- 05-react-weather/src/components/SearchCity.tsx
+++ 05-react-weather/src/components/SearchCity.tsx
@@ -17,6 +17,8 @@ const SearchCity: React.FC<IProps> = ({ onSearch }) => {
 		setCity("")
 	}
 
+	const tooFewCharacters = city.trim().length > 0 && city.trim().length < 3
+
 	return (
 		<div id="search-wrapper">
 			<form id="search-form" onSubmit={handleSubmit}>
@@ -30,10 +32,13 @@ const SearchCity: React.FC<IProps> = ({ onSearch }) => {
 					/>
 
 					<button
+						disabled={city.trim().length < 3}
 						type="submit"
 						className="btn btn-success"
 					>🔍</button>
 				</div>
+
+				{tooFewCharacters && <div className="form-text">Please enter at least 3 characters</div>}
 			</form>
 		</div>
 	)
