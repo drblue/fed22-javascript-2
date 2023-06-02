0bf69e186d23f0a01c95efbadb65a2892b12022b
Johan Nordström <johan@digitalvillage.se>
2023-06-02 15:11:25 +0200

    Search Hacker News HTML skeleton ☠️.

--- 07-hackernews/src/App.tsx
+++ 07-hackernews/src/App.tsx
@@ -2,8 +2,9 @@ import Container from 'react-bootstrap/Container'
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import HomePage from './pages/HomePage'
-import './assets/scss/App.scss'
 import NotFound from './pages/NotFound'
+import SearchPage from './pages/SearchPage'
+import './assets/scss/App.scss'
 
 const App = () => {
 	return (
@@ -13,7 +14,7 @@ const App = () => {
 			<Container className="py-3">
 				<Routes>
 					<Route path="/" element={<HomePage />} />
-					{/* <Route path="/search" element={} /> */}
+					<Route path="/search" element={<SearchPage />} />
 
 					<Route path="*" element={<NotFound />} />
 				</Routes>
