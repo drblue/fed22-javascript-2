09ff6238e68f66a9bb9559edc6b9e8950bea92ba
Johan Nordström <johan@digitalvillage.se>
2023-06-08 13:58:16 +0200

    Get data when URL changes.

--- 07-hackernews/src/pages/RandomDogPage.tsx
+++ 07-hackernews/src/pages/RandomDogPage.tsx
@@ -1,24 +1,42 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import Button from 'react-bootstrap/Button'
 import Image from 'react-bootstrap/Image'
 import { DogAPI_RandomImageResponse } from '../types'
 
 const RandomDogPage = () => {
 	const [data, setData] = useState<DogAPI_RandomImageResponse|null>(null)
+	const [url, setUrl] = useState<string|null>(null)
 
-	const getData = async () => {
-		const res = await axios.get<DogAPI_RandomImageResponse>("https://dog.ceo/api/breeds/image/random")
-		await new Promise(r => setTimeout(r, 3000))
+	const getData = async (resourceUrl: string) => {
+		const res = await axios.get<DogAPI_RandomImageResponse>(resourceUrl)
+		// await new Promise(r => setTimeout(r, 3000))
 		setData(res.data)
 	}
 
 	useEffect(() => {
-		getData()
-	}, [])
+		if (!url) {
+			return
+		}
+
+		getData(url)
+	}, [url])
 
 	return (
 		<>
 			<h1>A random doggo 🐶</h1>
+
+			<div>
+				<Button
+					variant="primary"
+					onClick={() => setUrl("https://dog.ceo/api/breeds/image/random")}
+				>Random Doggo</Button>
+
+				<Button
+					variant="primary"
+					onClick={() => setUrl("https://dog.ceo/api/breed/boxer/images/random")}
+				>Random Boxer Doggo</Button>
+			</div>
 
 			{!data && <p>Loading...</p>}
 
