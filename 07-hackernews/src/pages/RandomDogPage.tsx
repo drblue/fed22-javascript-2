bc0d0e4624de8c4e4b0153c231966fca79611eae
Johan Nordström <johan@digitalvillage.se>
2023-06-09 11:51:52 +0200

    useGetRandomDogImage (with support for a random image from a specific breed).

--- 07-hackernews/src/pages/RandomDogPage.tsx
+++ 07-hackernews/src/pages/RandomDogPage.tsx
@@ -2,38 +2,17 @@ import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button'
 import Image from 'react-bootstrap/Image'
 import Spinner from 'react-bootstrap/Spinner'
-import useGetData from '../hooks/useGetData'
-import { DogAPI_RandomImageResponse } from '../types'
+import useGetRandomDogImage from '../hooks/useGetRandomDogImage'
 
 const RandomDogPage = () => {
-	const { changeUrl, data, error, execute, isError, isLoading } =
-		useGetData<DogAPI_RandomImageResponse>()
+	const { data, error, execute, isError, isLoading } =
+		useGetRandomDogImage()
 
 	return (
 		<>
 			<h1>A random doggo 🐶</h1>
 
 			<div className="mb-3">
-				<Button
-					variant="primary"
-					onClick={() => changeUrl("https://dog.ceo/api/breeds/image/random")}
-				>Random Doggo</Button>
-
-				<Button
-					variant="primary"
-					onClick={() => changeUrl("https://dog.ceo/api/breed/boxer/images/random")}
-				>Random Boxer Doggo</Button>
-
-				<Button
-					variant="danger"
-					onClick={() => changeUrl("C:\\passwords.txt")}
-				>Haxx0r</Button>
-
-				<Button
-					variant="danger"
-					onClick={() => changeUrl("http://iuowrvenu9esrnvcu9senoiaercs.com")}
-				>Fail Army</Button>
-
 				<Button
 					variant="primary"
 					onClick={() => execute()}
