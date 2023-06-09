07fa4f317dbcb9fdef6bad40de85e1850ac7501c
Johan Nordström <johan@digitalvillage.se>
2023-06-09 11:40:08 +0200

    Forgot to re-add the URL to the Chuck Norris API.

--- 07-hackernews/src/hooks/useChuckNorrisJoke.ts
+++ 07-hackernews/src/hooks/useChuckNorrisJoke.ts
@@ -2,7 +2,7 @@ import { ChuckNorrisAPI_RandomJokeResponse } from "../types"
 import useGetData from "./useGetData"
 
 const useChuckNorrisJoke = () => {
-	return useGetData<ChuckNorrisAPI_RandomJokeResponse>()
+	return useGetData<ChuckNorrisAPI_RandomJokeResponse>("https://api.chucknorris.io/jokes/random")
 }
 
 export default useChuckNorrisJoke
